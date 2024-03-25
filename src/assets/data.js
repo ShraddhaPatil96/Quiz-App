375d7d4c53201b5c1b286c4c352c915ba07fe47e
GitDemo <patilshraddha564@gmail.com>
2024-03-25 12:34:59 +0530

    commit

---
+++ src/assets/data.js
@@ -0,0 +1,48 @@
+export const data = [
+    {
+        question: "Which River is called 'The Sorrow of the Bengal?'",
+        option1: "Sone",
+        option2: "Damodar",
+        option3: "Hoogly",
+        option4: "Kosi",
+        ans: 2,
+        
+    },
+
+    {
+        question: "Guwahati is situated on the bank of the river",
+        option1: "Teesta",
+        option2: "Brahmaputra",
+        option3: "Hoogly",
+        option4: "Sone",
+        ans: 2,
+    },
+
+    {
+        question: "The Ajanta painting belongs to which period?",
+        option1: "Harappa period",
+        option2: "Gupta period",
+        option3: "Buddhist period",
+        option4: "Mauryan period",
+        ans: 2,
+    },
+
+    {
+        question: "Which is the widest river of India?",
+        option1: "Brahmaputra",
+        option2: "Gomti",
+        option3: "Ganga",
+        option4: "Chambal",
+        ans: 1,
+    },
+
+    {
+        question: "Which is the tallest statue of India?",
+        option1: "Harmandir Sahib",
+        option2: "Hampi",
+        option3: "Statue of Unity",
+        option4: "Gomateshwar",
+        ans: 3,
+    },
+]
+
